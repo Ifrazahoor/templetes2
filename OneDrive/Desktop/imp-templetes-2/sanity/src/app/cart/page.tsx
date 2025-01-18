8c88be1a3a909656324ae0bb3c2906b269b5e010
Ifrazahoor <ifra8766@gmail.com>
2025-01-18 22:32:40 +0500

    first commit

---
+++ OneDrive/Desktop/imp-templetes-2/sanity/src/app/cart/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar} from "@/components/Navbar"
+import Cart from "@/components/cart"
+import Footer from "@/components/Footer"
+
+export default function Checkout() {
+    return (
+        <>
+        <Navbar/>
+        <Cart/>
+        <Footer/>
+        </>
+    )
+}
---
+++ OneDrive/Desktop/imp-templetes-2/sanity/src/app/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { sanityfetch } from "@/sanity/lib/fetch";
+import { sanityFetch } from "@/sanity/lib/live";
+import { allproducts } from "@/sanity/lib/queries";
+
+import { Navbar } from "@/components/Navbar";
+import Hero from "@/components/hero-banner"
+import Features from "@/components/features-section"
+import News from "@/components/newsletter-section"
+import Hero2 from "@/components/hero-section"
+import Footer from "@/components/Footer"
+type Product={
+  _id:string;
+  name:string;
+  description:string;
+  price:number;
+  imageurl:string
+}
+export default async function Home() {
+  const products: Product[] = await sanityfetch({query:allproducts })
+  
+  return(
+    <div>
+        <Navbar/>
+    <Hero />
+    <Features/>
+    <Hero2/>
+    <News/>
+    <Footer/>
+
+{
+  products.map((product)=>(
+<div className="border p-4 rounded-lg flex flex-col items-center" key={product._id}>
+<Image src={product.imageurl} alt={product.name} className="w-60" width={500} height={500}/> 
+<h2 className="text-xl font-bold text-center">
+  {product.name}
+ </h2>
+ <p className="text-center">
+  {product.description}
+</p>
+<p className="text-center">
+  {product.price}
+</p>
+  </div>
+  ))
+}
+      </div>
+  
+  )
+}
